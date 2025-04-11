76a8837319608dd4b4940a58e13d7520c9707417
Md.Mosarof Hossen <mosarofhossen5228@gmail.com>
2025-04-11 10:12:40 +0600

    linklist

---
+++ linklist.cpp
@@ -0,0 +1,743 @@
+Skip to main content
+Google Classroom
+Classroom
+CSE2203 & CSE2204_Data Structure Theory & Sessional
+31B2
+Home
+Calendar
+Enrolled
+To-do
+T
+TOC(31B2) - Spring 25
+Friday 4 pm
+M
+Math 2201: Complex Variables & Probability
+Mathematics IV, Friday South-607_6PM
+C
+CSE2203 & CSE2204_Data Structure Theory & Sessional
+31B2
+D
+DLD
+31B2
+Archived classes
+Settings
+Stream
+Classwork
+People
+CSE2203 & CSE2204_Data Structure Theory & Sessional
+31B2
+Upcoming
+Woohoo, no work due soon!
+
+Announce something to your class
+
+Announcement: "***Lab Test announcement (Marks = 30)…"
+Tasnia Haque Kheya
+Created Apr 6Apr 6
+***Lab Test announcement (Marks = 30)
+ As I said earlier, a lab test(Coding Test) will be taken on Friday(11-04-2025) on (LabTime : 2:00PM).
+
+Practice the following code files which I had provided already in this classroom for the Lab Test:
+array.c
+BubbleSort.cpp
+linear_search.cpp
+binary_search.cpp
+stack.cpp
+queue.cpp
+linked_list.cpp
+Note : Those who have laptops, please bring your own laptops on the day of lab test as there can be issues on the lab's computers while running the codes.
+
+Add class comment…
+
+
+Announcement: "Theory + Sessional Lecture 07 for Final:"
+Tasnia Haque Kheya
+Created Apr 5Apr 5
+Theory + Sessional Lecture 07 for Final:
+
+queue.cpp
+Text
+
+linked_list.cpp
+Text
+
+Linked List _ (Queue + Linked List Code).mp4
+Video
+
+Add class comment…
+
+
+Announcement: "Class Link :…"
+Tasnia Haque Kheya
+Created Apr 5Apr 5
+Class Link : 
+https://bdren.zoom.us/j/99053300220
+
+Add class comment…
+
+
+Announcement: "Theory Lecture 06 for Final :"
+Tasnia Haque Kheya
+Created Apr 4Apr 4
+Theory Lecture 06 for Final :
+
+Graph.pptx
+Microsoft PowerPoint
+
+Graph.mp4
+Video
+
+Add class comment…
+
+
+Announcement: "Class Link:…"
+Tasnia Haque Kheya
+Created Apr 4Apr 4
+Class Link: 
+https://bdren.zoom.us/j/97991211223?pwd=tfknR9nFyDZjfhI4lrIgAt2ILAr6JE.1
+
+Add class comment…
+
+
+Announcement: "CT Question Link:…"
+Tasnia Haque Kheya
+Created Mar 22Mar 22
+CT Question Link:
+https://docs.google.com/forms/d/e/1FAIpQLSfXEjnAWXCDLL-vgy2dzZfuGs0SB4sW7VKFP4oLgo-VUsHQvg/viewform?usp=sharing
+Time: 30 minutes
+1 class comment
+
+Yasin AlamMar 22
+মেমম একটু টাইম বাড়িয়ে দেনন।আমি মাত্র খেয়াল করছি পরীক্ষা। 😭😭
+
+Add class comment…
+
+
+Announcement: "Quiz Link :…"
+Tasnia Haque Kheya
+Created Mar 22Mar 22
+Quiz Link :
+https://quiz.everestwebdeals.co/?form=de5e7e7e7b59b6a9c6c779b899b99937
+
+Add class comment…
+
+
+Announcement: "Class Link :…"
+Tasnia Haque Kheya
+Created Mar 22Mar 22
+Class Link : https://bdren.zoom.us/j/93625846192
+(Class will start from 9:20 PM)
+Everyone please install camscanner or any other PDF maker app to create pdf of your CT answer script.
+
+Add class comment…
+
+
+Announcement: "Theory Lecture 05 for Final :"
+Tasnia Haque Kheya
+Created Mar 21Mar 21 (Edited Apr 5)
+Theory Lecture 05 for Final :
+
+Linked List Part One.mp4
+Video
+
+Linked List Part Two.mp4
+Video
+
+Linked List Updated.pptx
+Microsoft PowerPoint
+
+Add class comment…
+
+
+Announcement: "Class Link:…"
+Tasnia Haque Kheya
+Created Mar 21Mar 21
+Class Link:
+https://bdren.zoom.us/j/93375545914
+
+Add class comment…
+
+
+Announcement: "A class test and A lab quiz will be…"
+Tasnia Haque Kheya
+Created Mar 17Mar 17 (Edited Mar 17)
+A class test and A lab quiz will be taken online on Saturday(22-03-2025) from 9:15PM - 10:15PM.  As these will be  online tests, so keep a stable internet connection. If someone wll miss any test, no 2nd chance will be given to attempt the test. 
+
+***CT announcement (CT - 02) (Marks = 10) 
+Time : 25 minutes
+Class Test Syllabus:
+Theory Lecture 01 + 02 for Final
+Note:  This is the final CT of this course, no further CT will be taken. The students who were absent or got less marks in CT - 01 are requested to attend this CT because I'll count the best one from CT - 01 and CT - 02.
+
+***Lab Quiz announcement (Marks = 30)
+Time: 25 minutes
+Lab Quiz Syllabus:
+Code of 3rd Lab/Sessional
+Code of 4rth Lab/Sessional 
+Code of Binary Search Algorithm and Stack
+Note : I'll provide 30  mcq questions.
+
+Add class comment…
+
+
+Announcement: "Theory Lecture 04 for Final :"
+Tasnia Haque Kheya
+Created Mar 17Mar 17 (Edited Mar 17)
+Theory Lecture 04 for Final :
+
+Lecture 04.mp4
+Video
+
+Binary Search Tree.pptx
+Microsoft PowerPoint
+
+Add class comment…
+
+
+Announcement: "As discussed earlier, Make Up class of…"
+Tasnia Haque Kheya
+Created Mar 17Mar 17 (Edited Mar 17)
+As discussed earlier, Make Up class of 08-03-2025 will be taken today(17-03-2025) from 9:30PM - 10:30PM.
+Class Link : https://bdren.zoom.us/j/93789599431
+
+Add class comment…
+
+
+Announcement: "Theory Lecture 03 for Final :"
+Tasnia Haque Kheya
+Created Mar 15Mar 15 (Edited Mar 17)
+Theory Lecture 03 for Final :
+
+Lecture 03.mp4
+Video
+
+Tree.pptx
+Microsoft PowerPoint
+
+Add class comment…
+
+
+Announcement: "https://bdren.zoom.us/j/96526363614"
+Tasnia Haque Kheya
+Created Mar 15Mar 15
+https://bdren.zoom.us/j/96526363614
+
+Add class comment…
+
+
+Announcement: "Sorry, due to heavy rain, electricity…"
+Tasnia Haque Kheya
+Created Mar 8Mar 8
+Sorry, due to heavy rain, electricity cut off and I was facing network issues. Therefore, I could not conduct today's class properly. I'll take a make up class as soon as possible. Time and date of the make up class will be informed later.
+
+Add class comment…
+
+
+Announcement: "https://bdren.zoom.us/j/97547916335"
+Tasnia Haque Kheya
+Created Mar 8Mar 8
+https://bdren.zoom.us/j/97547916335
+
+Add class comment…
+
+
+Announcement: "Today's class will start from 9:15 PM…"
+Tasnia Haque Kheya
+Created Mar 8Mar 8
+Today's class will start from 9:15 PM instead of  8:00 PM due to Taraweeh Prayer. 
+
+(Note: If I start the class from 10:00PM which was discussed in yesterday's lab, it'll become very late to end the class. Therefore, the authority suggests me to start the class from 9:15PM.)
+
+Add class comment…
+
+
+Announcement: "Theory Lecture 01 + 02 for Final :"
+Tasnia Haque Kheya
+Created Mar 7Mar 7 (Edited Mar 17)
+Theory Lecture 01 + 02 for Final :
+
+Lecture 01.mp4
+Video
+
+Lecture 02.mp4
+Video
+
+Queue.pptx
+Microsoft PowerPoint
+
+Circular Queue.pdf
+PDF
+
+Add class comment…
+
+
+Announcement: "Mid Term Marks : Note:…"
+Tasnia Haque Kheya
+Created Mar 4Mar 4 (Edited Mar 8)
+Mid Term Marks :
+
+Note:  07/03/2025(Friday) lab a khata dekhano hobe.
+
+Mid_Marks_Data Structure_Updated.pdf
+PDF
+
+Add class comment…
+
+
+Announcement: "https://bdren.zoom.us/j/95776757507"
+Tasnia Haque Kheya
+Created Mar 1Mar 1
+https://bdren.zoom.us/j/95776757507
+
+Add class comment…
+
+
+Announcement: "https://bdren.zoom.us/j/96033535102"
+Tasnia Haque Kheya
+Created Feb 28Feb 28
+https://bdren.zoom.us/j/96033535102
+
+Add class comment…
+
+
+Announcement: "The class of…"
+Tasnia Haque Kheya
+Created Feb 28Feb 28
+The class of 28/02/2025(Friday)(Tomorrow) from 2:00pm to 4:00pm will be held online.
+
+Add class comment…
+
+
+Announcement: "https://bdren.zoom.us/j/92829355944"
+Tasnia Haque Kheya
+Created Feb 22Feb 22
+https://bdren.zoom.us/j/92829355944
+
+Add class comment…
+
+
+Announcement: "1st CT Marks: Note : The students who…"
+Tasnia Haque Kheya
+Created Feb 6Feb 6
+1st CT Marks: 
+
+Note : The students who did not get desire marks, don't be upset. I'll take another CT which I previously told and will count the best one. Just Focus on tomorrow's mid exam. I suggest, everyone should check the CT answer script which I posted yesterday. Good Luck!
+
+CT_Marks_Data Structure.pdf
+PDF
+1 class comment
+
+Washim Akram ShishirFeb 6
+Thanks mam . It was nice marking.
+
+Add class comment…
+
+
+Announcement: "CT Question and Answer:"
+Tasnia Haque Kheya
+Created Feb 5Feb 5 (Edited Feb 6)
+CT Question and Answer:
+Displaying 31B2.png
+31B2.png
+
+Class_Test _Solve_31B2.pdf
+PDF
+
+2a.c
+Text
+
+2b.c
+Text
+
+Add class comment…
+
+
+Announcement: "Code of Binary Search Algorithm and…"
+Tasnia Haque Kheya
+Created Feb 1Feb 1
+Code of Binary Search Algorithm and Stack :
+
+binary_search.cpp
+Text
+
+stack.cpp
+Text
+
+Add class comment…
+
+
+Announcement: "Theory_Online_Class Lecture 05 (Review…"
+Tasnia Haque Kheya
+Created Feb 1Feb 1
+Theory_Online_Class Lecture 05 (Review Class):
+
+Review Class.mp4
+Video
+
+Add class comment…
+
+
+Announcement: "https://bdren.zoom.us/j/96332354186"
+Tasnia Haque Kheya
+Created Feb 1Feb 1
+https://bdren.zoom.us/j/96332354186
+
+Add class comment…
+
+
+Announcement: "***CT announcement Date: 31 January,…"
+Tasnia Haque Kheya
+Created Jan 24Jan 24 (Edited Jan 25)
+***CT announcement
+Date: 31 January, 2025
+Time: 2:05 pm (Lab Time)
+Syllabus:  Array (Theory + Code)
+
+Add class comment…
+
+
+Announcement: "Playlist link of Bulbul Ahamed Sir If…"
+Tasnia Haque Kheya
+Created Jan 25Jan 25 (Edited Jan 27)
+Playlist link of Bulbul Ahamed Sir
+If anyone face difficulties to understand any topic, you can watch the videos from this playlist.
+
+Data Structure - YouTube
+https://youtube.com/playlist?list=PLg5dIxhq7aY4Mfvn3pwHXwyOGoSmPaqES&si=3ifsYsPHsgTbpQ8c
+
+Add class comment…
+
+
+Announcement: "Theory_Online_Class Lecture 04 (Stack) :"
+Tasnia Haque Kheya
+Created Jan 25Jan 25 (Edited Jan 27)
+Theory_Online_Class Lecture 04 (Stack) :
+
+Stack.pptx
+Microsoft PowerPoint
+
+STACK_PDF.pdf
+PDF
+
+Stack Part One.mp4
+Video
+
+Stack Part Two.mp4
+Video
+
+Add class comment…
+
+
+Announcement: "https://us04web.zoom.us/j/75531244765?p…"
+Tasnia Haque Kheya
+Created Jan 25Jan 25 (Edited Jan 25)
+https://us04web.zoom.us/j/75531244765?pwd=VnFxFz9Arl4oTL2li2ahOHvb56Rdnw.1
+https://us04web.zoom.us/j/72117389314?pwd=cTmvRKLbBQPDhCeIznU9ofAOggSGMV.1
+
+Add class comment…
+
+
+Announcement: "Code of 4rth Lab/Sessional :"
+Tasnia Haque Kheya
+Created Jan 25Jan 25 (Edited Jan 25)
+Code of 4rth Lab/Sessional :
+
+linear_search.cpp
+Text
+
+binary_search_function.txt
+Text
+
+Add class comment…
+
+
+Announcement: "নবীনবরণ অনুষ্ঠান সংক্রান্ত নোটিশ CSE…"
+Tasnia Haque Kheya
+Created Jan 25Jan 25
+নবীনবরণ অনুষ্ঠান সংক্রান্ত নোটিশ  
+
+CSE এবং BBA প্রোগ্রামের সকল শিক্ষার্থীকে জানানো যাচ্ছে যে, আগামী ১লা ফেব্রুয়ারি ২০২৫ তারিখে সোনারগাঁও ইউনিভার্সিটি অডিটোরিয়ামে একটি নবীনবরণ অনুষ্ঠান আয়োজিত হবে।  
+
+যেসকল শিক্ষার্থী সাংস্কৃতিক অনুষ্ঠানে (যেমন: নাচ, গান, কমেডি) অংশগ্রহণ করতে আগ্রহী, তাদেরকে আগামীকাল (২৬/০১/২০২৫) তারিখের মধ্যে নিম্নোক্ত WhatsApp নম্বরে যোগাযোগ করার অনুরোধ করা হচ্ছে।  
+
+আপনাদের প্রাণবন্ত অংশগ্রহণ অনুষ্ঠানটি সফল করতে গুরুত্বপূর্ণ ভূমিকা রাখবে।  
+
+ধন্যবাদান্তে,  
+
+মোঃ ফজলে রাব্বী  
+লেকচারার, DBA, SU
+যোগাযোগ (W/A): 01984562286
+
+Add class comment…
+
+
+Announcement: "Theory_Online_Class Lecture 03 (Linear…"
+Tasnia Haque Kheya
+Created Jan 18Jan 18 (Edited Jan 27)
+Theory_Online_Class Lecture 03 (Linear + Binary Search) :
+
+Searching Algorithms.pptx
+Microsoft PowerPoint
+
+Searching Algorithms.mp4
+Video
+
+Add class comment…
+
+
+Announcement: "Code of 3rd Lab/Sessional : To…"
+Tasnia Haque Kheya
+Created Jan 18Jan 18
+Code of 3rd Lab/Sessional :
+To understand Bubble Sort more clearly for both Theory and Sessional, I suggest to watch the video.
+
+BubbleSort.cpp
+Text
+
+7.3 Bubble Sort Algorithm| Data Structures Tutorials
+YouTube video • ‪35 minutes‬
+
+Add class comment…
+
+
+Announcement: "https://meet.google.com/tpv-yxoq-nqj…"
+Tasnia Haque Kheya
+Created Jan 18Jan 18 (Edited Jan 24)
+https://meet.google.com/tpv-yxoq-nqj
+https://us04web.zoom.us/j/74669955573?pwd=LAsuEZIGhmRZ95bYgAgBGWdZZO9EPT.1
+
+Add class comment…
+
+
+Announcement: "Code of 2nd Lab/Sessional :"
+Tasnia Haque Kheya
+Created Jan 17Jan 17 (Edited Jan 17)
+Code of 2nd Lab/Sessional :
+
+array.c
+Text
+
+Add class comment…
+
+
+Announcement: "Mid Syllabus : 1. Introduction Slide…"
+Tasnia Haque Kheya
+Created Jan 11Jan 11 (Edited Jan 27)
+Mid Syllabus :
+
+1. Introduction Slide (Introduction of Data Structure)
+2. Array 
+3. Bubble Sort Algorithm
+4. Linear Search
+5. Binary Search
+6. Stack
+
+Add class comment…
+
+
+Announcement: "Theory_Online_Class Lecture 02 (Bubble…"
+Tasnia Haque Kheya
+Created Jan 11Jan 11 (Edited Feb 5)
+Theory_Online_Class Lecture 02 (Bubble Sort Algorithm):
+
+Theory_Lecture_02.mp4
+Video
+Link
+https://www.javatpoint.com/bubble-sort
+
+BubbleSort_Example.pdf
+PDF
+
+BubbleSort_Example_two.pdf
+PDF
+
+Add class comment…
+
+
+Announcement: "join the online class :…"
+Tasnia Haque Kheya
+Created Jan 11Jan 11
+join the online class :
+https://us04web.zoom.us/j/72845158924?pwd=7FBrEw724Kugf4y51UybcP4d4yGy5c.1
+
+Add class comment…
+
+
+Announcement: "To understand Array more clearly for…"
+Tasnia Haque Kheya
+Created Jan 10Jan 10 (Edited Jan 10)
+To understand Array more clearly for both Theory and Sessional, I suggest to read the javapoint document and watch the videos.
+Link
+https://www.javatpoint.com/data-structure-array
+
+1.1 Arrays in Data Structure | Declaration, Initialization, Memory representation
+YouTube video • ‪22 minutes‬
+
+1.2 Array Operations - Traversal, Insertion | Explanation with C Program | DSA Course
+YouTube video • ‪28 minutes‬
+
+1.3 Array Operations | Deletion from Array | Explanation with Code | Data Structure
+YouTube video • ‪14 minutes‬
+
+Add class comment…
+
+
+Announcement: "Theory_Online_Class Lecture 01 (Array) :"
+Tasnia Haque Kheya
+Created Jan 10Jan 10 (Edited Jan 24)
+Theory_Online_Class Lecture 01 (Array) :
+
+Theory_Lecture_01.mp4
+Video
+
+Lecture_01_Array.pptx
+Microsoft PowerPoint
+
+Add class comment…
+
+
+Announcement: "Slide - 01 (Introduction of Data…"
+Tasnia Haque Kheya
+Created Jan 7Jan 7 (Edited Feb 5)
+Slide - 01 (Introduction of Data Structure)
+
+Introduction.pptx
+Microsoft PowerPoint
+
+Add class comment…
+
+
+Announcement: "Mark Distribution (Sessional/Lab): Lab…"
+Tasnia Haque Kheya
+Created Jan 7Jan 7 (Edited Apr 6)
+Mark Distribution (Sessional/Lab):
+Lab Test : 30
+Lab Quiz : 30
+Assignment - 30
+Attendance - 10
+
+Add class comment…
+
+
+Announcement: "Mark Distribution (Theory): Mid Term :…"
+Tasnia Haque Kheya
+Created Jan 7Jan 7 (Edited Jan 7)
+Mark Distribution (Theory):
+Mid Term : 30
+Final : 40
+Attendance : 10
+Class Test : 10 (2 CT will be taken and I'll count the best mark between the two)
+Assignment : 10
+1 class comment
+
+Washim Akram ShishirJan 7
+Ok Mam
+
+Add class comment…
+
+
+Announcement: "বাংলাদেশ প্রিমিয়ার লীগ(বি পি এল) এর…"
+Tasnia Haque Kheya
+Created Jan 7Jan 7
+বাংলাদেশ প্রিমিয়ার লীগ(বি পি এল) এর সহযোগিতায় তারুণ্যের উৎসব - ২০২৫ উদযাপন  উপলক্ষে বিভিন্ন ধরনের কর্মসূচির আয়োজন করা হয়েছে।
+তরুণদেরকে উদ্ভাবনী ও উদ্যোক্তা কর্মী হিসেবে গড়ে তোলার লক্ষ্যে সোনারগাঁও বিশ্ববিদ্যালয় আয়োজন করতে যাচ্ছে " তারুণ্য মেলা - ২০২৫"।  যেখানে শিক্ষার্থীদের স্টল দেয়ার ব্যবস্থা রয়েছে এবং উদ্ভাবনীয় সৃজনশীলতার ভিত্তিতে পুরস্কার প্রদান করা হবে।
+এই মর্মে শিক্ষার্থীদের আগামী ৯ ই জানুয়ারী এর মধ্যে প্রস্তাবনা জমা দেয়ার আহ্বান জানানো হচ্ছে।
+যোগাযোগ: নাঈমা খান তারিন
+প্রভাষক, ব্যবসায় প্রশাসন বিভাগ
+w app: 01825966168
+
+Add class comment…
+
+
+Announcement: "Please Join the Online class of…"
+Tasnia Haque Kheya
+Created Jan 4Jan 4 (Edited Jan 24)
+Please Join the Online class of 04/01/2025 (8:00pm - 10:00pm) in this meeting link : 
+https://us04web.zoom.us/j/74308165229?pwd=EiVDLMcOiZrpfXtFe8eHzelB0h1ZFa.1
+
+Meeting ID:  743 0816 5229
+Passcode: 1XCmp8
+
+https://meet.google.com/qqk-aykd-hdg
+
+Add class comment…
+
+#include <iostream>
+using namespace std;
+
+// Node structure
+struct Node {
+    int data;
+    Node* next;
+};
+
+// Insert Operation
+void insert(Node*& head, int value) {
+    Node* newNode = new Node{value, NULL};
+
+    if (head == NULL) {
+        head = newNode;
+        return;
+    }
+
+    Node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+}
+
+// Delete Operation
+void deleteNode(Node*& head, int value) {
+    Node* temp = head;
+    Node* prev = NULL;
+
+    while (temp != NULL) {
+        if (temp->data == value) {
+            if (prev == NULL) {
+                // Deleting head
+                head = head->next;
+            } else {
+                // Deleting non-head
+                prev->next = temp->next;
+            }
+            delete temp;
+            return;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+
+    cout << "Value not found!" << endl;
+}
+
+// Traverse Operation
+void traverse(Node* head) {
+    cout << "Linked List: ";
+    while (head != NULL) {
+        cout << head->data << " -> ";
+        head = head->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main() {
+
+    Node* head = NULL;
+
+    // Insert
+    insert(head, 10);
+    insert(head, 20);
+    insert(head, 30);
+
+    traverse(head); // Output: 10 -> 20 -> 30 -> NULL
+
+    // Delete
+    deleteNode(head, 20);
+
+    traverse(head); // Output: 10 -> 30 -> NULL
+
+    return 0;
+}
+linked_list.cpp
+Displaying linked_list.cpp.
